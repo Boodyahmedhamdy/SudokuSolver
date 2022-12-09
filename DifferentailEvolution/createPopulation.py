4c46e2a17d4aee9c5b8cf1855eff2edc2fbaa0c2
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2022-12-09 16:38:27 +0200

    added doc string

--- DifferentailEvolution/createPopulation.py
+++ DifferentailEvolution/createPopulation.py
@@ -9,9 +9,14 @@ population creation phase
 
 """
 
-def createPopulation(numberofpopulation=10):
 
-    pop = np.zeros((numberofpopulation,9, 9), dtype=np.int64)
+def createPopulation(numberofpopulation=10):
+    """
+
+    :param numberofpopulation: length of the population returned by this function
+    :return: population with given length defualt = 10 individuals
+    """
+    pop = np.zeros((numberofpopulation, 9, 9), dtype=np.int64)
     for z in range(numberofpopulation):
         for x in range(9):
             for y in range(9):
@@ -22,9 +27,15 @@ def createPopulation(numberofpopulation=10):
 
     return pop
 
+
 # For testing only
 def createPopulationFromBoard(board, numberofpopulation=10):
+    """
 
+    :param board: board to take saved positions from
+    :param numberofpopulation: length of population
+    :return: population of given length based on given board
+    """
     pop = np.zeros((numberofpopulation, 9, 9), dtype=np.int64)
     savedPositions = getSavedPositions(board)
 
@@ -38,9 +49,9 @@ def createPopulationFromBoard(board, numberofpopulation=10):
 
     return pop
 
+
 print("nothing")
 board = createBoard()
 pop = createPopulationFromBoard(board)
 
 print(pop)
-
